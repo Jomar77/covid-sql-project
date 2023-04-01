58b080003bafa3b15ec4eb7e66fed5987af94f8c
JOMAR NACORDA <jnacorda04@gmail.com>
2023-04-01 20:37:02 +1300

    added pop vs vaccinated

--- covid.sql
+++ covid.sql
@@ -6,10 +6,9 @@ Order by 3,4
 --FROM CovidProject..CovidVaccine
 --Order by 3,4
 
-Select Location, date, max(total_cases), total_deaths, population
+Select *
 From CovidProject..Coviddeath
-group by location, date
-Order by 1,2
+
 
 Select Location, date,population, total_cases,( CAST(total_deaths as float) / cast (total_cases  as float)) *100 as deathPercent
 From CovidProject..Coviddeath
@@ -24,3 +23,29 @@ From CovidProject..Coviddeath
 --where location like '%new zealand%'
 group by Location, Population
 Order by infectedrate desc
+
+Select Location, max(cast(total_deaths as int)) as totaldeathcount
+From CovidProject..Coviddeath
+where continent is not null and location like 'new zealand'
+group by Location
+Order by totaldeathcount desc
+
+Select location, max(cast(total_deaths as int)) as totaldeathcount
+From CovidProject..Coviddeath
+where continent is null
+group by location
+Order by totaldeathcount desc
+
+Select SUM(new_cases) as total_cases, Sum(Cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int)) / sum(new_cases) *100 as deathpercentage
+From CovidProject..Coviddeath
+where continent is not null
+--group by date
+Order by 1,2
+
+
+Select *
+From CovidProject..Coviddeath dea
+join CovidProject..CovidVaccine vac
+	on dea.location = vac.location
+	and dea.date = dea.date
+
